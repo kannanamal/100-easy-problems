c62cc3788dd686173ede4e260521722c4c6a4a01
AMAL S <kannanamal202004@gmail.com>
2025-08-20 00:14:49 +0530

    smpleinterest.py
    can calculate the simple interest

---
+++ smpleinterest.py
@@ -0,0 +1,5 @@
+p=int(input("Enter the Principal amount:"))
+r=float(input("Enter the rate of interest:"))
+t=int(input("Enter the time in years:"))
+si=(p*r*t)/100
+print("the simple interest is:",si)
